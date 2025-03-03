c303ed2a6f90dc4829b2eee69715203398c3616a
DarshanDeshmukh10 <deshmukhdarshan447@gmail.com>
2025-03-03 14:43:20 +0530

    header complete

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,7 +1,96 @@
-import React from "react";
-
+import React,{useState,useEffect} from 'react'
+import Wrapper from './Wrapper';
+import Link from 'next/link';
+import Menu from './Menu';
+import MenuMobile from './MenuMobile';
+//fetchDataFromApi
+//icons
+import {IoMdHeartEmpty} from "react-icons/io";
+import {BsCart} from "react-icons/bs";
+import {BiMenuAltRight} from "react-icons/bi";
+import {VscChromeClose} from "react-icons/vsc";
+//import { fetchDataFromApi } from '@/utils/api';
 const Header = () => {
-    return <div className="text">Header</div>;
-};
+   
+    const[mobileMenu,setMobileMenu]=useState(false);
+    const[showCatMenu,setShowCatMenu]=useState(false);
+    //y axis will be zero
+    const [show,setShow]=useState("translate-y-0");
+    const [lastScrollY,setLastScrollY]=useState(0);
+    const [categories,setcategories]=useState(null);
+   //for header animation on scroll
+   const controlNavbar=()=>{
+      if(window.scrollY>200)
+      {
+        if(window.scrollY> lastScrollY && !mobileMenu){
+          setShow("-translate-y-[80px]")
+         }
+         else{
+          setShow("shadow-sm")
+         }
+      }else{
+        setShow("translate-y-0")
+      }
+      setLastScrollY(window.scrollY)
+   }
 
-export default Header;
+   useEffect (()=>{
+    window.addEventListener("scroll", controlNavbar)
+    return()=>{
+      window.removeEventListener("scroll",controlNavbar)
+    }
+   },[lastScrollY]) 
+   
+   useEffect(()=>{
+//    fetchCategories();
+   },[])
+//    const fetchCategories=async()=>{
+//       const {data}=await fetchDataFromApi('/api/categories?populate=*')
+//       setcategories(data)
+//    }
+  return (
+    <div className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
+        <Wrapper className='h-[60px] flex justify-between items-center'>
+          <Link href="/">
+            <img src="/logo.svg" className='w-[40px] md:w-[60px] ' />
+          </Link>
+          
+          <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu} categories={categories}/>
+
+          {mobileMenu&&<MenuMobile showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu} />}
+        
+          <div className='flex items-center gap-2 text-black'>
+          
+            {/*wishlist icon */}
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <IoMdHeartEmpty className="text-[19px] md:text-[24px] "/>
+              <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md-text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>15</div>
+            </div>
+          
+            {/*cart icon */}
+            <Link href='/cart'>
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px] "/>
+              <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>5</div>
+            </div>
+            </Link>
+            {/*Mobile icon */}
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2 md:hidden">
+              {mobileMenu ? (
+                <VscChromeClose className="text-[16px]" onClick={()=> setMobileMenu(false)}/>
+              ) : (
+                <BiMenuAltRight className='text-[20px]' onClick={()=> setMobileMenu(true)}/>
+              )}
+            </div>
+
+          </div>
+           
+         
+
+          
+        </Wrapper>
+    </div>
+  )
+};  
+
+export default Header
