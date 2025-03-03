a382b3265f2af89ee9dd3a7454cd202d2e67363d
DarshanDeshmukh10 <deshmukhdarshan447@gmail.com>
2025-03-03 15:44:08 +0530

    carasol complete

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,17 @@
+import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
+HeroBanner
 export default function Home() {
-    return <main className="h-[2000px]">Home page....</main>;
+    return <main className="h-[2000px]">
+    <HeroBanner/>
+    <Wrapper>
+    <div className="text-center max-w-[1200px] max-auto-my-[50px] md:my-[80px]">
+        <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">Causing For Your Miles</div>
+        <div className="text-md md:text-xl">
+        Nike Air cushioning reduces the weight of the shoe without reducing performance. The lighter the shoe is, the less energy athletes put out during their performance. 
+        </div>
+        </div>
+         
+    </Wrapper>
+    </main>;
 }
